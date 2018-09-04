5bef01a0de35d7d5cb9ad2eb62fb22e01bef97b6
Cristian Leonardo Fonseca Soto <cristian.lfs@gmail.com>
2018-09-04 18:30:08 -0500

    create model form person and bill, update uml

--- bill/models.py
+++ bill/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from person.models import Person
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Bill(models.Model):
+    """
+    Models from bill
+    """
+    event_name = models.CharField('Nombre del evento', max_length=255)
+    seller = models.OneToOneField(User,verbose_name='Vendedor',on_delete=models.PROTECT)
+    number_book = models.IntegerField('Numero de talonario')
+    person = models.OneToOneField(Person,verbose_name='Cliente',on_delete=models.PROTECT)
+    number_bill = models.IntegerField('Numero de factura')
+    quantity = models.IntegerField('Cantidad de fotos')
+    sale_date = models.DateField('Fecha de venta')
+    event_date = models.DateField('Fecha del evento')
+    observations = models.TextField('Observaciones')
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Factura'
+        verbose_name_plural = 'Facturas'
+
+
--- person/models.py
+++ person/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    """
+    Model from person
+    """
+    first_name = models.CharField('Nombres', max_length=255)
+    last_name = models.CharField('Apellidos', max_length=255)
+    address = models.CharField('Dirección', max_length=255)
+    phone = models.CharField('Telefono',max_length=11)
+    payment = models.DecimalField('Abono',max_digits=100,decimal_places=10)
+    career = models.CharField('Carrera-Especialidad',max_length=255, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Persona'
+        verbose_name_plural = 'Personas'
