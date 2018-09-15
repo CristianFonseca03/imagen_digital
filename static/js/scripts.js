e2b006a56707828fdf10d490ba8f1a2c54e89577
Cristian Leonardo Fonseca Soto <cristian.lfs@gmail.com>
2018-09-15 18:34:48 -0500

    Create responsive navbar from movil, change logo and login template

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,6 @@
+function randomNumber(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
 $("#menu-toggle").click(function (e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
@@ -9,17 +12,36 @@ $("#menu-toggle").click(function (e) {
         $("#button-menu").removeClass("fa-angle-double-left");
     }
 });
+$("#menuDropdown").click(function (e) {
+    if ($("#collapseMenu").hasClass('show')) {
+        $("#button-dropdown").addClass("fa-angle-double-down");
+        $("#button-dropdown").removeClass("fa-angle-double-up");
+    } else {
+        $("#button-dropdown").addClass("fa-angle-double-up");
+        $("#button-dropdown").removeClass("fa-angle-double-down");
+    }
+});
 $(window).resize(function () {
     var i = $(window).width()
     if (i < 768) {
         $("#wrapper").removeClass("toggled");
-    } if (i > 768) {
-        $("#wrapper").addClass("toggled");
+    }
+    if ($("#wrapper").hasClass('toggled')) {
+        $("#button-menu").addClass("fa-angle-double-left");
+        $("#button-menu").removeClass("fa-angle-double-right");
+    } else {
+        $("#button-menu").addClass("fa-angle-double-right");
+        $("#button-menu").removeClass("fa-angle-double-left");
     }
 });
 $(document).ready(function () {
+    const colors = ["bg-primary","bg-danger","bg-success","bg-info","bg-purple","bg-pink"]
+    $("#color-1").addClass(colors[randomNumber(0,5)]);
+    $("#color-2").addClass(colors[randomNumber(0,5)]);
+    $("#color-3").addClass(colors[randomNumber(0,5)]);
     var i = $(window).width()
     if (i > 768) {
         $("#wrapper").addClass("toggled");
     }
+
 });
