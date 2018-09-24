29fb517f24065c3e68a06bcecfe7b7b6364a0c81
Cristian Leonardo Fonseca Soto <cristian.lfs@gmail.com>
2018-09-24 15:10:02 -0500

    Create DeleteView from users

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from users import views
-from users.views import UserDetailView, HomeView, UsersListView
+from users.views import UserDetailView, HomeView, UsersListView, UserDeleteView
 
 app_name = 'users'
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path(route='logout/', view=views.logout_view, name='logout'),
     path(route='find/<str:username>/', view=UserDetailView.as_view(), name='detail'),
     path(route='list-users/', view=UsersListView.as_view(), name='list-users'),
+    path(route='delete/<str:username>/', view=UserDeleteView.as_view(), name='delete'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 # Django classes
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from django.views.generic import DetailView, TemplateView, ListView
+from django.views.generic import DetailView, TemplateView, ListView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib import messages
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+
 
 # Models
 from django.contrib.auth import get_user_model
@@ -32,7 +35,7 @@ def login_view(request):
 
 @permission_required(('user.is_authenticated','user.is_superuser'))
 def signup_view(request):
-    """Sign up view"""
+    """Sign up view."""
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
@@ -50,13 +53,13 @@ def signup_view(request):
 
 @login_required
 def logout_view(request):
-    """Logout a user"""
+    """Logout a user."""
     logout(request)
     return redirect('user:login')
 
 
 class UserDetailView(LoginRequiredMixin, DetailView):
-    """User Detail View"""
+    """User Detail View."""
     template_name = "users/detail.html"
     slug_field = "username"
     slug_url_kwarg = "username"
@@ -75,3 +78,16 @@ class UsersListView(PermissionRequiredMixin, ListView):
     model = User
     context_object_name = 'users'
     template_name = 'users/list_users.html'
+
+class UserDeleteView(PermissionRequiredMixin,SuccessMessageMixin,DeleteView):
+    """Delete user."""
+    permission_required = ('user.is_authenticated', 'user.is_superuser')
+    model = User
+    slug_field = "username"
+    slug_url_kwarg = "username"
+    template_name = 'users/delete_user.html'
+    success_url = reverse_lazy('user:home')
+    success_message = 'Perfil eliminado satisfactoriamente'
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request,self.success_message)
+        return super(UserDeleteView,self).delete(request, *args, **kwargs)
